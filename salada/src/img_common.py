01ad1b75a0c649af614835f403098bef39e2608c
shiraiyusuke <memetta2002@yahoo.co.jp>
2017-11-18 20:50:13 +0900

    [add] img_common

--- salada/src/img_common.py
+++ salada/src/img_common.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*- 
 import os
 import sys
-import numpy as np
-import pandas as pd
+import cv2
 
+def resize_image(image, size):
+    """
 
-def main():
-    pass
+    :param image: np.array
+    :param size: tuple(x, y)
+    :return: np.array
+    """
+    resize_img = cv2.resize(image, size)
+    return resize_img
 
+def display_image(image):
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-if __name__ == '__main__':
-    main()
+def save_image(image, save_name):
+    cv2.imwrite(image, save_name)
